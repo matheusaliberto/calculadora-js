be694e7c232a0c4410b87a956e01d6ebaf8e2caa
Matheus H. Aliberto de Francisco <matheushenrique612@gmail.com>
2022-03-13 21:36:42 -0300

    updates

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
-function calculadora(x){
-    window.alert(x)
+function exibir(x){
+    var ftela =document.getElementById('resultado')
+    ftela.innerHTML += x
+}
+function apagar(){
+    window.alert('em construção')
+}
+function resetar(){
+    var tela =document.getElementById('resultado')
+    tela.innerHTML = "0"
+}
+function calcular(){
+    var ftela = document.getElementById('resultado')
+    var tela = ftela.value
+    var tela2 = Array.from(tela);
+    window.alert(tela2)
+    window.alert(typeof tela2[0])
 }
