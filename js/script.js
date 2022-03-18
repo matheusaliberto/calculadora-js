12066dade097c793eaeeba2cf327947b93d8d744
Matheus H. Aliberto de Francisco <matheushenrique612@gmail.com>
2022-03-18 11:52:48 -0300

    versão final

--- js/script.js
+++ js/script.js
@@ -1,36 +1,21 @@
 function exibir(x){
-    var tela =document.getElementById('resultado')
-    tela.innerHTML += x
+    var tela = document.getElementById('resultado');
+    tela.innerHTML += x;
 }
 function apagar(){
-    var tela = document.getElementById('resultado')
-    var telavalor = document.getElementById('resultado').firstChild.nodeValue
-    var teladel = telavalor.slice(0,-1)
-
-    tela.innerHTML = teladel
+    var tela = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = tela.substring(0,tela.length-1);
 }
 function resetar(){
-    var tela =document.getElementById('resultado')
-    tela.innerHTML = ""
+    var tela =document.getElementById('resultado');
+    tela.innerHTML = "";
 }
 function calcular(){
-    var tela = document.getElementById('resultado')
-    var telavalor =document.getElementById('resultado').firstChild.nodeValue
-    var valorarray = telavalor.split('')
-
-    for(let i=0;i<valorarray.length;i++){
-        
-        switch(valorarray[i]){
-            case '+':
-                //cond
-            case '-':
-                //cond
-            case '/':
-                //cond
-            case '*':
-                //cond
-            default:
-                    
-        }
+    var tela = document.getElementById('resultado').innerHTML;
+    if(tela){
+        document.getElementById('resultado').innerHTML = eval(tela);
+    }
+    else{
+        window.alert('Digite um valor');
     }
 }
